00628f4478b2cfa961506ad896a42728d422fbc2
Yash tiwari <123789297+Y-tiwari@users.noreply.github.com>
2023-02-27 22:20:54 +0530

    Add files via upload

---
+++ html,css,javascript,jqury/task.js
@@ -0,0 +1,56 @@
+//task-4//
+
+$(function() {
+    $("table tr:nth-child(odd)")
+        .addClass("zebrastripe");
+});
+
+// task-5//
+onclick="window.print();"
+$('a.printPage').click(function(){
+    window.print();
+    return false;
+});
+
+//task-6//
+$(document).ready(function(){
+    $('p').each(function() {
+    var html = $(this).html();
+    var word = html.substr(0, html.indexOf(" "));           
+    var rest = html.substr(html.indexOf(" "));
+    $(this).html(rest).prepend($("<span/>").html("<b>"+word+"</b>") );
+});
+});
+
+//task-7//
+		function addText() {
+			$(document).ready(function() {
+
+				var object = {
+					id: "divID",
+					class: "divClass",
+					css: {
+						"color": "Red"
+					}
+				};
+				var addition = $("<div>", object);
+				addition.html("Added text GFG");
+				$("#parent").append(addition);
+			});
+		}
+        //task-8//
+        function move(elem) {
+            if ($(elem).parent().attr("id") == "nonSelected") {
+            $(elem).detach().appendTo("#selected");
+            } else {
+            $(elem).detach().appendTo("#nonSelected");
+            }
+        }
+
+        //task-9//
+        function new_element(){
+            $("#header ul").append('<li>Java</li>');
+              $("#header ul").append('<li>php</li>');
+              $("#header ul").append('<li>C</li>');
+            }
+            
